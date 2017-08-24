eab07d217ff60e92da482ba0fce8c4cd23e14293
Karthik Rajasekaran <karajas@microsoft.com>
2017-08-24 10:58:04 -0700

    Allow initialise component

--- init-tools.cmd
+++ init-tools.cmd
@@ -5,18 +5,32 @@ set INIT_TOOLS_LOG=%~dp0init-tools.log
 if [%PACKAGES_DIR%]==[] set PACKAGES_DIR=%~dp0packages\
 if [%TOOLRUNTIME_DIR%]==[] set TOOLRUNTIME_DIR=%~dp0Tools
 set DOTNET_PATH=%TOOLRUNTIME_DIR%\dotnetcli\
+if NOT exist "%DOTNET_PATH%" mkdir "%DOTNET_PATH%"
 if [%DOTNET_CMD%]==[] set DOTNET_CMD=%DOTNET_PATH%dotnet.exe
 if [%BUILDTOOLS_SOURCE%]==[] set BUILDTOOLS_SOURCE=https://dotnet.myget.org/F/dotnet-buildtools/api/v3/index.json
 set /P BUILDTOOLS_VERSION=< "%~dp0BuildToolsVersion.txt"
 set BUILD_TOOLS_PATH=%PACKAGES_DIR%Microsoft.DotNet.BuildTools\%BUILDTOOLS_VERSION%\lib\
 set INIT_TOOLS_RESTORE_PROJECT=%~dp0init-tools.msbuild
 set BUILD_TOOLS_SEMAPHORE=%TOOLRUNTIME_DIR%\%BUILDTOOLS_VERSION%\init-tools.completed
+if [%COMPONENT_SOURCE%]==[] set COMPONENT_SOURCE=https://dotnet.myget.org/F/dotnet-buildtools/api/v3/index.json
+if [%COMPONENT_DIR%]==[] set COMPONENT_DIR=%~dp0component\
+set COMPONENT_ONLY=0
 
 :: if force option is specified then clean the tool runtime and build tools package directory to force it to get recreated
-if [%1]==[force] (
-  if exist "%TOOLRUNTIME_DIR%" rmdir /S /Q "%TOOLRUNTIME_DIR%"
-  if exist "%PACKAGES_DIR%Microsoft.DotNet.BuildTools" rmdir /S /Q "%PACKAGES_DIR%Microsoft.DotNet.BuildTools"
+if NOT [%1]==[] (
+  if [%1]==[force] (
+    if exist "%TOOLRUNTIME_DIR%" rmdir /S /Q "%TOOLRUNTIME_DIR%"
+    if exist "%PACKAGES_DIR%Microsoft.DotNet.BuildTools" rmdir /S /Q "%PACKAGES_DIR%Microsoft.DotNet.BuildTools"
+    goto :beforedotnetrestore
+  )
+  set COMPONENT=%1
+  set COMPONENT_ONLY=1
+  ::if exist "%PACKAGES_DIR%%COMPONENT%" rmdir /S /Q "%PACKAGES_DIR%%COMPONENT%"
+  if NOT exist "%COMPONENT_DIR%" mkdir "%COMPONENT_DIR%"
+  goto :afterdotnetrestore
 )
+
+:beforedotnetrestore
 
 :: If semaphore exists do nothing
 if exist "%BUILD_TOOLS_SEMAPHORE%" (
@@ -45,10 +59,20 @@ if NOT exist "%DOTNET_LOCAL_PATH%" (
 
 :afterdotnetrestore
 
+if [%COMPONENT_ONLY%]==[1] (
+  echo Restoring component %COMPONENT%...
+  echo Running: "%DOTNET_CMD%" restore "%INIT_TOOLS_RESTORE_PROJECT%" --no-cache --packages %PACKAGES_DIR% --source "%COMPONENT_SOURCE%" /p:ComponentPackage=%COMPONENT% /p:TargetFramework= /p:AllowWildCard=true >> "%INIT_TOOLS_LOG%"
+  "%DOTNET_CMD%" restore "%INIT_TOOLS_RESTORE_PROJECT%" --no-cache --packages %PACKAGES_DIR% --source "%COMPONENT_SOURCE%" /p:ComponentPackage=%COMPONENT% /p:TargetFramework= /p:AllowWildCard=true
+  powershell -NoProfile -ExecutionPolicy unrestricted -Command "$dirToCopy = Get-ChildItem %PACKAGES_DIR%%COMPONENT% | ? { $_.PSIsContainer } | sort CreationTime -desc | select -f 1; Copy-Item %PACKAGES_DIR%%COMPONENT%\$($dirToCopy.ToString())\lib %COMPONENT_DIR%\%COMPONENT% -recurse"
+  echo Done initializing component.
+  goto :done
+)
+
 if exist "%BUILD_TOOLS_PATH%" goto :afterbuildtoolsrestore
 echo Restoring BuildTools version %BUILDTOOLS_VERSION%...
 echo Running: "%DOTNET_CMD%" restore "%INIT_TOOLS_RESTORE_PROJECT%" --no-cache --packages %PACKAGES_DIR% --source "%BUILDTOOLS_SOURCE%" /p:BuildToolsPackageVersion=%BUILDTOOLS_VERSION% >> "%INIT_TOOLS_LOG%"
-call "%DOTNET_CMD%" restore "%INIT_TOOLS_RESTORE_PROJECT%" --no-cache --packages %PACKAGES_DIR% --source "%BUILDTOOLS_SOURCE%" /p:BuildToolsPackageVersion=%BUILDTOOLS_VERSION% >> "%INIT_TOOLS_LOG%"
+call "%DOTNET_CMD%" restore "%INIT_TOOLS_RESTORE_PROJECT%" --no-cache --packages %PACKAGES_DIR% --source "%BUILDTOOLS_SOURCE%" /p:ComponentPackage="Microsoft.DotNet.BuildTools" /p:BuildToolsPackageVersion=%BUILDTOOLS_VERSION% >> "%INIT_TOOLS_LOG%"
+
 if NOT exist "%BUILD_TOOLS_PATH%init-tools.cmd" (
   echo ERROR: Could not restore build tools correctly. 1>&2
   goto :error
@@ -68,6 +92,8 @@ if not [%INIT_TOOLS_ERRORLEVEL%]==[0] (
 :: Create semaphore file
 echo Done initializing tools.
 echo Init-Tools.cmd completed for BuildTools Version: %BUILDTOOLS_VERSION% > "%BUILD_TOOLS_SEMAPHORE%"
+
+:done
 exit /b 0
 
 :error
